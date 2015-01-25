b717bfcc9b27a12c53364cbe6d4d6a0c4c3328c8
maxild <mmaxild@gmail.com>
2015-01-25 01:53:15 +0100

    Platform specific settings/preferences via package

--- scripts/install.ps1
+++ scripts/install.ps1
@@ -14,16 +14,16 @@ function Get-Basename ($path) { Split-Path -parent $path }
 
 # http://stackoverflow.com/questions/817794/find-out-whether-a-file-is-a-symbolic-link-in-powershell
 function Test-SymLink([string]$path) {
-	if (!(Test-Path $path)) {
-		return $false;
-	}
-  	$file = Get-Item $path -Force -ea 0
-  	return [bool]($file.Attributes -band [IO.FileAttributes]::ReparsePoint)
+    if (!(Test-Path $path)) {
+      return $false;
+    }
+    $file = Get-Item $path -Force -ea 0
+    return [bool]($file.Attributes -band [IO.FileAttributes]::ReparsePoint)
 }
 
 function New-SymLink ($link, $target)
 {
-    if (Test-Path -Path $target -PathType Container)
+    if ( (Get-Item $target) -is [System.IO.DirectoryInfo] )
     {
         $command = "cmd /c mklink /d"
     }
@@ -37,7 +37,7 @@ function New-SymLink ($link, $target)
 
 function Remove-SymLink ($link)
 {
-    if (Test-Path -Path $link -PathType Container)
+    if (Test-Path -Path "'$link'" -PathType Container)
     {
       # If you have a symbolic link that is a directory (made with mklink /d)
       # then using del will delete all of the files in the target directory
@@ -56,31 +56,48 @@ function Remove-SymLink ($link)
 }
 
 function Main {
-	# Path to git repo Packages/User folder (source)
-	$scriptsFolder = Get-Basename $PSCommandPath
-	$root = Get-Basename $scriptsFolder
-	$gitRepoUserFolder = Join-Path $root "User"
+  # Path to git repo Packages/User folder (source)
+  $scriptsFolder = Get-Basename "$PSCommandPath"
+  $root = Get-Basename "$scriptsFolder"
+  $userOsSettingsFolder="User (OS Settings)"
+  $userFolder="User"
+  $gitRepoUserOsSettingsFolder = Join-Path "$root" "Packages\$userOsSettingsFolder"
+  $gitRepoUserFolder = Join-Path "$root" "$userFolder"
 
-	# Path to Sublime Text Packages/User folder
-	$sublimePackagesFolder = "${env:AppData}\Sublime Text 3\Packages"
-	$sublimeUserFolder = Join-Path "$sublimePackagesFolder" "User"
+  # Path to Sublime Text Packages/User folder
+  $sublimePackagesFolder = "${env:AppData}\Sublime Text 3\Packages"
+  $sublimeUserOsSettingsFolder = Join-Path "$sublimePackagesFolder" "$userOsSettingsFolder"
+  $sublimeUserFolder = Join-Path "$sublimePackagesFolder" "$userFolder"
 
-	if ( !(Test-Path -PathType Container $sublimeUserFolder) ) {
-		echo "The folder $sublimeUserFolder does not exist."
-	}
-	elseif ( !(Test-SymLink $sublimeUserFolder) ) {
-		echo "The folder $sublimeUserFolder does exist and the folder is not a symlink."
+  if ( Test-SymLink "$sublimeUserOsSettingsFolder" ) {
+      echo "The folder '$sublimeUserOsSettingsFolder' is already a symlink."
+  }
+  elseif ( Test-Path "$sublimeUserOsSettingsFolder") {
+      echo "Error: The folder '$sublimeUserOsSettingsFolder' already does exist, but it is NOT a symlink."
+      exit
+  }
+  else {
+      New-SymLink "$sublimeUserOsSettingsFolder" "$gitRepoUserOsSettingsFolder"
+  }
 
-		# rename/backup existing Packages/User folder
-		Rename-Item -Path "$sublimeUserFolder" -NewName "_User"
+  if ( !(Test-Path -PathType Container $sublimeUserFolder) ) {
+    echo "Error: The folder '$sublimeUserFolder' does not exist."
+    exit
+  }
+  elseif ( !(Test-SymLink $sublimeUserFolder) ) {
+    echo "The folder '$sublimeUserFolder' does exist and the folder is not a symlink......yet!"
 
-		# create symlink for Packages/User folder
-		#Remove-SymLink "$sublimeUserFolder"
-		New-SymLink "$sublimeUserFolder" "$gitRepoUserFolder"
-	}
-	else {
-		echo "The folder $sublimeUserFolder is already a symlink."
-	}
+    # rename/backup existing Packages/User folder
+    Rename-Item -Path "$sublimeUserFolder" -NewName "_User"
+    echo "Renamed 'Packages/User' folder to 'Packages/_User' (i.e. created a backup)."
+
+    # create symlink for Packages/User folder
+    #Remove-SymLink "$sublimeUserFolder"
+    New-SymLink "$sublimeUserFolder" "$gitRepoUserFolder"
+  }
+  else {
+    echo "The folder '$sublimeUserFolder' is already a symlink."
+  }
 }
 
 Main
